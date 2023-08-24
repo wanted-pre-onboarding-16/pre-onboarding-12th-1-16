cad0950d6cd7330020c69682eb6e78e2075e2542
SeungGukYoo <dbtmd878@gmail.com>
2023-08-24 10:59:13 +0900

    feat: '/'페이지에서 바로 로그인 페이지로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
   const navigation = useNavigate();
   useEffect(() => {
     if (localStorage.getItem('jwt_token')) navigation('/todo');
+    navigation('/signin');
   }, [navigation]);
-  return (
-    <div>
-      <h1>Wanted Front-end Todo List</h1>
-      <div>
-        <Link to={'/signin'}>Sign In</Link>
-        <Link to={'/signup'}>Sign Up</Link>
-      </div>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
