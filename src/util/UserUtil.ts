29ee531479a7c689c3d24ba1496594128dbb27c2
apeachicetea <eodbszla@gmail.com>
2023-08-24 10:48:55 +0900

    refactor: API call 분리

--- src/util/UserUtil.ts
+++ src/util/UserUtil.ts
@@ -1,31 +1,11 @@
-import axios from 'axios';
+import { api } from './Api';
 
 export const SignInHandle = async (email: string, password: string) => {
-  const result = await axios({
-    url: 'https://www.pre-onboarding-selection-task.shop/auth/signin',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: {
-      email,
-      password,
-    },
-  });
+  const result = await api.post('/auth/signin', { email, password });
   return result;
 };
 
 export const SignUpHandle = async (email: string, password: string) => {
-  const result = await axios({
-    url: 'https://www.pre-onboarding-selection-task.shop/auth/signup',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: {
-      email,
-      password,
-    },
-  });
+  const result = await api.post('/auth/signup', { email, password });
   return result;
 };
