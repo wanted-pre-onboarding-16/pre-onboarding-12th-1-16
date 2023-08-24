2a227173a981530f345beb597e4ac0e842de59b5
SeungGukYoo <dbtmd878@gmail.com>
2023-08-24 16:35:35 +0900

    feat: todo 스타일 추가

--- src/components/Todo/Todo.styled.ts
+++ src/components/Todo/Todo.styled.ts
@@ -2,12 +2,11 @@ import styled from 'styled-components';
 
 export const List = styled.li`
   display: flex;
-
   margin: 2rem 0;
   padding: 0.5rem 0;
   border-bottom: 2px;
   border-style: solid;
-  border-color: #eee;
+  border-color: ${props => props.theme.colors.deepWhite};
 `;
 
 export const TodoWrapper = styled.div`
@@ -25,8 +24,6 @@ export const Input = styled.input`
   min-width: 250px;
   border: 0;
   height: 36px;
-  /* border-color: #38b2ac; /* border-teal-500의 일반적인 값입니다. 실제 프로젝트의 색상 값에 따라 변경될 수 있습니다. */
-  /* border-bottom-width: 2px; */
   padding: 0;
   outline: none;
 `;
@@ -36,16 +33,20 @@ export const Text = styled.p`
   min-width: 250px;
   vertical-align: middle;
   line-height: 1.5;
-  color: #eee;
+  color: ${props => props.theme.colors.deepWhite};
 `;
 export const ModifyBtn = styled.button`
   width: 100%;
   background-color: transparent;
   margin-right: 4px;
-  border: 1px solid #eee;
-  color: #eee;
+  border: 1px solid ${props => props.theme.colors.deepWhite};
+  color: ${props => props.theme.colors.deepWhite};
   padding: 2px 7px;
   border-radius: 0.25rem;
+  transition: color 0.2s;
+  &:hover {
+    color: black;
+  }
 `;
 export const DeleteBtn = styled(ModifyBtn)``;
 export const UpdateBtn = styled(ModifyBtn)``;
